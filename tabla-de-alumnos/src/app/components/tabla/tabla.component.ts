67b1e2e4a2d84a4759ae88ec591f3cab4c6eb0e7
Burixer85 <dvd.sevillano@gmail.com>
2024-10-16 22:59:12 +0200

    Ejercicio terminado (Error orden de columnas al añadirlas)

--- tabla-de-alumnos/src/app/components/tabla/tabla.component.ts
+++ tabla-de-alumnos/src/app/components/tabla/tabla.component.ts
@@ -1,25 +1,26 @@
 import { Component } from '@angular/core';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
 export interface PeriodicElement {
   numeroAlumno: number;
-  name: string;
+  nombre: string;
   apellidos: string;
   NIF: String;
-  Edad: number;
-  CursoMatriculado: string;
+  edad: number;
+  cursoMatriculado: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {numeroAlumno: 1, name: 'Hydrogen', apellidos: 'Pérez', NIF: '11111111A', Edad: 19, CursoMatriculado: '2024-2025'},
-  {numeroAlumno: 2, name: 'Helium', apellidos: 'Rodriguez', NIF: '22222222B', Edad: 20, CursoMatriculado: '2024-2025'},
-  {numeroAlumno: 3, name: 'Lithium', apellidos: 'García', NIF: '33333333C', Edad: 21, CursoMatriculado: '2024-2025'},
-  {numeroAlumno: 4, name: 'Beryllium', apellidos: 'Sanchez', NIF: '44444444D', Edad: 22, CursoMatriculado: '2024-2025'},
-  {numeroAlumno: 5, name: 'Boron', apellidos: 'Pineda', NIF: '55555555E', Edad: 23, CursoMatriculado: '2024-2025'},
-  {numeroAlumno: 6, name: 'Carbon', apellidos: 'Moreno', NIF: '66666666F', Edad: 24, CursoMatriculado: '2024-2025'},
-  {numeroAlumno: 7, name: 'Nitrogen', apellidos: 'Gutierrez', NIF: '77777777G', Edad: 25, CursoMatriculado: '2024-2025'},
-  {numeroAlumno: 8, name: 'Oxygen', apellidos: 'Rubio', NIF: '88888888H', Edad: 26, CursoMatriculado: '2024-2025'},
-  {numeroAlumno: 9, name: 'Fluorine', apellidos: 'Hernandez', NIF: '99999999H', Edad: 27, CursoMatriculado: '2024-2025'},
-  {numeroAlumno: 10, name: 'Neon', apellidos: 'Prados', NIF: '12345678I', Edad: 28, CursoMatriculado: '2024-2025'},
+  {numeroAlumno: 1, nombre: 'Jesus', apellidos: 'Pérez', NIF: '11111111A', edad: 19, cursoMatriculado: '2024-2025'},
+  {numeroAlumno: 2, nombre: 'Manuel', apellidos: 'Rodriguez', NIF: '22222222B', edad: 20, cursoMatriculado: '2024-2025'},
+  {numeroAlumno: 3, nombre: 'Pedro', apellidos: 'García', NIF: '33333333C', edad: 21, cursoMatriculado: '2024-2025'},
+  {numeroAlumno: 4, nombre: 'Marcos', apellidos: 'Sanchez', NIF: '44444444D', edad: 22, cursoMatriculado: '2024-2025'},
+  {numeroAlumno: 5, nombre: 'Luis', apellidos: 'Pineda', NIF: '55555555E', edad: 23, cursoMatriculado: '2024-2025'},
+  {numeroAlumno: 6, nombre: 'Adrian', apellidos: 'Moreno', NIF: '66666666F', edad: 24, cursoMatriculado: '2024-2025'},
+  {numeroAlumno: 7, nombre: 'Alvaro', apellidos: 'Gutierrez', NIF: '77777777G', edad: 25, cursoMatriculado: '2024-2025'},
+  {numeroAlumno: 8, nombre: 'David', apellidos: 'Rubio', NIF: '88888888H', edad: 26, cursoMatriculado: '2024-2025'},
+  {numeroAlumno: 9, nombre: 'Raul', apellidos: 'Hernandez', NIF: '99999999H', edad: 27, cursoMatriculado: '2024-2025'},
+  {numeroAlumno: 10, nombre: 'Lucas', apellidos: 'Prados', NIF: '12345678I', edad: 28, cursoMatriculado: '2024-2025'},
 ];
 
 @Component({
@@ -28,6 +29,14 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrl: './tabla.component.css'
 })
 export class TablaComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['numeroAlumno', 'nombre', 'apellidos', 'NIF', 'edad', 'cursoMatriculado'];
   dataSource = ELEMENT_DATA;
+
+  setAll(evento: MatCheckboxChange, columna: string) {
+    if (evento.checked) {
+      this.displayedColumns.push(columna);
+    } else {
+      this.displayedColumns = this.displayedColumns.filter(col => col !== columna);
+    }
+  }
 }
